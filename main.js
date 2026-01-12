f0cb94d990775eb31e3b1b9b73c504466f6b27b0
athamjon <atham4@gmail.com>
2026-01-12 18:52:24 +0500

    done

---
+++ main.js
@@ -0,0 +1,23 @@
+let users = {};
+
+
+for (let i = 1; i <= 10; i++) {
+  let name = prompt(`Введите имя пользователя ${i}`);
+  let age = prompt(`Введите возраст пользователя ${i}`);
+
+  users[i] = {
+    name: name,
+    age: 0 
+  };
+}
+
+
+for (let key in users) {
+  console.log(`Пользователь ${key}
+Ваше имя  ${users[key].name}
+Ваше Возраст  ${users[key].age}`);
+}
+
+
+
+console.log(users);
